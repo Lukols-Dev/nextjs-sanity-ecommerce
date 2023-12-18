83842eb11b06fc782cd9cba50567250c1d8a24b6
Lukols-Dev <lukols.dev@gmail.com>
2023-12-18 15:13:45 +0100

    add fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import AddToCardBtn from "./buttons/AddToCartBtn";
 import CheckoutNowBtn from "./buttons/CheckoutNow";
+import { Fragment } from "react";
 
 interface ListingCardProps {
   data: any;
@@ -9,24 +10,26 @@ interface ListingCardProps {
 
 const Card: React.FC<ListingCardProps> = ({ data }) => {
   return (
-    <div key={data._id} className="col-span-1 cursor-pointer group">
-      <Link href={`/${data.slug}`}>
-        <div className="flex flex-col gap-2 w-full h-[500px] justify-between">
-          <div className="aspect-square h-full  w-full relative overflow-hidden">
-            <Image
-              fill
-              className="object-cover h-full w-full group-hover:scale-110 transition"
-              src={data.imageUrl}
-              alt="Listing"
-            />
+    <Fragment key={data._id}>
+      <div className="col-span-1 cursor-pointer group">
+        <Link href={`/${data.slug}`}>
+          <div className="flex flex-col gap-2 w-full h-[500px] justify-between">
+            <div className="aspect-square h-full  w-full relative overflow-hidden">
+              <Image
+                fill
+                className="object-cover h-full w-full group-hover:scale-110 transition"
+                src={data.imageUrl}
+                alt="Listing"
+              />
+            </div>
+            <p className="font-semibold text-lg">{data.name}</p>
+            <p className="font-semibold text-lg">${data.price}</p>
+            <CheckoutNowBtn />
+            <AddToCardBtn item={data} />
           </div>
-          <p className="font-semibold text-lg">{data.name}</p>
-          <p className="font-semibold text-lg">${data.price}</p>
-          <CheckoutNowBtn />
-          <AddToCardBtn item={data} />
-        </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
+    </Fragment>
   );
 };
 
--- src/components/ListCards.tsx
+++ src/components/ListCards.tsx
@@ -11,7 +11,7 @@ const ListCards: FC<ListCardsProps> = async ({ data, route }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
       {data.map((product: simplifiedProduct) => (
-        <Card data={product} key={product._id} />
+        <Card data={product} />
       ))}
     </div>
   );
