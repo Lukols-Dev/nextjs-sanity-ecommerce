b1d4843c847537b518fb7fc68a6c24f026cbe70f
Lukols-Dev <lukols.dev@gmail.com>
2023-12-29 17:53:22 +0100

    add sanity shop metadata and icon

--- src/components/navbar/Logo.tsx
+++ src/components/navbar/Logo.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import { ShoppingBagIcon } from "@heroicons/react/24/outline";
+import { BuildingStorefrontIcon } from "@heroicons/react/24/outline";
 
 const Logo = () => {
   return (
-    <Link href={"/"}>
-      <ShoppingBagIcon className="text-black w-6 h-6" />
+    <Link href={"/"} className="flex gap-2 font-semibold">
+      <BuildingStorefrontIcon className="text-black w-6 h-6" />
+      Sanity Shop
     </Link>
   );
 };
